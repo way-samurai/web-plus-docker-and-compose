31dc0b8691219fc9fb7457cdfec128eee4addd75
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-07-23 12:00:12 +0300

    Обновил конфигурацию окружения бекенда configuration.ts

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,12 +1,12 @@
 export default () => ({
-  port: 3001,
+  port: process.env.SERVER_PORT,
   database: {
-    host: 'localhost',
-    port: 5432,
-    username: 'student',
-    password: 'student',
-    database: 'kupipodariday',
-    shema: 'kupipodariday',
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    shema: process.env.POSTGRES_SHEMA,
   },
   secretKey: process.env.JWT_SECRET || 'secret-key',
 });
