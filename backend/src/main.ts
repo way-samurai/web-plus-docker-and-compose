de7b6b58817471a0c07ca2dfa723bc6945b669d7
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-07-24 20:26:50 +0300

    fix: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,10 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    credentials: true,
-    origin: true,
-  });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.use(cookieParser());
