912b70d27247911ed3f9b1a744e9f493b9125ac7
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-07-24 03:37:07 +0300

    --amend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://kupiprodai-samurai.nomoredomains.xyz',
+    origin: 'https://kupiprodai-samurai.nomoredomains.xyz',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
