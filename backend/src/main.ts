7eb8b2be0648e0175cab5089b1298e61bf447e93
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-07-24 03:25:03 +0300

    Обновил cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://kupiprodai-samurai.nomoredomains.xyz',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.use(cookieParser());
